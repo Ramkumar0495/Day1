7f5914b35a831c482786558a928441b7927b1503
srikanth1198 <srivenkat1198@gmail.com>
2023-02-11 12:21:44 +0530

    state

--- src/test/java/org/test/Samplemas.java
+++ src/test/java/org/test/Samplemas.java
@@ -12,6 +12,18 @@ public class Samplemas {
 	private void chennai() {
 		System.out.println("chennai");
 
+	}//sri
+	
+	private void tamilnadu() {
+		System.out.println("tamil");
+
 	}
+	private void kerala() {
+	System.out.println("malaiyalam");
+
+	}
+	private void karnataka() {
+		System.out.println("karnataka");
+		}
 
 }
