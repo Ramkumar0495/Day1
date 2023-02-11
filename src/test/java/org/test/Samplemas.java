43395570cf8771fec40e93eed28690245b0869d4
Ramkumar <ramkumar0495@gmail.com>
2023-02-11 11:56:33 +0530

    login

---
+++ src/test/java/org/test/Samplemas.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Samplemas {
+	private void ramnad() {
+		System.out.println("ramnad");
+
+	}
+	private void madurai() {
+		System.out.println("madurai");
+
+	}
+	private void chennai() {
+		System.out.println("chennai");
+
+	}
+
+}
